06de79f0b4a9c0742fd468d33edb7155019757f0
Shubhangi <shubhangis20@navgurukul.org>
2021-07-26 19:33:25 +0530

    changes added

--- data.js
+++ data.js
@@ -1,29 +1,54 @@
 /** @format */
+const fs = require("fs");
+
 var dic = {};
-const getdata = (req, res) => {
-  const data = req.body.data;
-  var input = data.trim().split(" ");
-  console.log(input);
-  if (input[0] == "set") {
-    if (input.length === 3) {
-      dic[input[1]] = input[2];
-      // console.log(dic);
-      return { status: "Ok \n" };
-    } else {
-      return { res: "wrong number of arguments for 'set' command \n" };
-    }
-  } else if (input[0] == "get") {
-    if (input.length === 2) {
-      const output = dic[input[1]];
-      if (output) {
-        return { status: output + "\n" };
-      }
-       return { status: "data not available\n" };
-    } else {
-      return { res: "wrong number of arguments for 'get' command \n" };
-    }
+const read = () => {
+  data_ = fs.readFileSync("./data.json", "utf8");
+  return data_;
+};
+
+const write = (key,value)=>{
+    let data1 = read()
+    let data = JSON.parse(data1)
+    data[key]=value;
+    let jsonData = JSON.stringify(data)
+    fs.writeFileSync('./data.json',jsonData)
+  return "data saved";
+}
+
+const setdata = (req, res) => {
+  const data = req.body.input;
+  if (data.length === 3) {
+    dic[data[1]] = data[2];
+    // console.log(dic);
+    return { status: "Ok \n" };
   } else {
-    return { res: "unknown command \n" };
+    return { res: "wrong number of arguments for 'set' command \n" };
   }
 };
-module.exports = getdata;
+const getdata = (req, res) => {
+  const data = req.body.input;
+   if (data.length === 2) {
+     const output = dic[data[1]];
+     if (output) {
+       return { status: output + "\n" };
+     }
+     return { status: "data not available\n" };
+   } else {
+     return { res: "wrong number of arguments for 'get' command \n" };
+   }
+}
+const savedata = (req, res) => {
+  const data = req.body.input;
+  if (data.length === 3) {
+    dic[data[1]] = data[2];
+    let key = data[1];
+    let value = data[2];
+    const output=write(key,value)
+    return { status: output+ "\n" };
+  } else {
+    return { res: "wrong number of arguments for 'set' command \n" };
+  }
+  
+}
+module.exports = { setdata, getdata, savedata };
--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 /** @format */
 
-const getdata = require("./data");
+const { setdata } = require("./data");
+const { getdata } = require("./data");
+const { savedata } = require("./data");
 var net = require("net");
 var server = net.createServer(function (socket) {
   socket.on("data", function (data) {
     var data = data.toString("utf8");
-    // console.log(data.length)
+    var input = data.trim().split(" ");
     let req = {
       body: {
-        data,
+        input,
       },
     };
-    const datas = getdata(req);
-    socket.write("server reply " + Object.values(datas));
+    if (input[0] == "set") {
+      const output = setdata(req);
+      socket.write("server reply " + Object.values(output));
+    } else if (input[0] == "get") {
+      const output = getdata(req);
+      socket.write("server reply " + Object.values(output));
+    } else if (input[0] == "save") {
+      const output = savedata(req);
+      socket.write("server reply " + Object.values(output));
+    } else if (input[0] == "quit") {
+      socket.end("server is stoped \n");
+    } else {
+      socket.write("unknown command \n");
+    }
   });
 });
 server.listen(7777);
